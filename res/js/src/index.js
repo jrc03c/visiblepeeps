df9f7ebef81cb3e4fcd45bda73bdc2607a5548e1
jrc03c <joshrcastle@gmail.com>
2018-08-22 16:41:38 -0500

    ...

--- res/js/src/index.js
+++ res/js/src/index.js
@@ -1,3 +1,43 @@
+let Vue = require("vue/dist/vue");
+let VueRouter = require("vue-router");
+let Vuex = require("vuex");
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+let Firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/database");
+
 window.onload = function(){
-	console.log("heh.");
+	Firebase.initializeApp({
+		apiKey: "AIzaSyDahkALUP2HDnuLqiXOt-ScOPobhqFoW84",
+		authDomain: "visiblewomen-net.firebaseapp.com",
+		databaseURL: "https://visiblewomen-net.firebaseio.com",
+		projectId: "visiblewomen-net",
+		storageBucket: "",
+		messagingSenderId: "532732660193"
+	});
+	
+	let routes = [
+		{path: "/", component: require("./components/vp-landing.vue")},
+		{path: "/about", component: require("./components/vp-about.vue")},
+		{path: "/submit", component: require("./components/vp-submit.vue")},
+		{path: "/manage", component: require("./components/vp-manage.vue")},
+		{path: "/login", component: require("./components/vp-login.vue")},
+	];
+	
+	let router = new VueRouter({routes});
+	
+	let store = new Vuex.Store({
+		state: {},
+		getters: {},
+		mutations: {},
+		actions: {},
+	});
+	
+	let app = new Vue({
+		el: "#app",
+		router,
+		store,
+	});
 };

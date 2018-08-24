c2e59fbfbe0335e0ea5b11d070f0e509643ebbea
jrc03c <joshrcastle@gmail.com>
2018-08-24 10:00:09 -0500

    ...

---
+++ res/js/toggle-menu.js
@@ -0,0 +1,27 @@
+$(window).ready(function() {
+    if ($(window).width() > 1050) {
+        $('.mobile-nav').css('display','none');
+        $('#side-menu').show();
+    }
+    else {
+        $('.mobile-nav').css('display','block');
+        $('#side-menu').hide();
+    }
+});
+
+$(window).resize(function() {
+    if ($(window).width() > 1050) {
+        $('.mobile-nav').css('display','none');
+        $('#side-menu').show();
+    }
+    else {
+        $('.mobile-nav').css('display','block');
+        $('#side-menu').hide();
+    }
+});
+
+$(".mobile-nav").click(function(e){
+    e.preventDefault();
+    $("#side-menu").slideToggle("fast");   
+      return false;
+});

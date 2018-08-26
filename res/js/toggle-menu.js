176fd633838d856c3cedd799b319319676812842
AWerchmeister <42685208+AWerchmeister@users.noreply.github.com>
2018-08-26 17:59:56 +0200

    s

--- res/js/toggle-menu.js
+++ res/js/toggle-menu.js
@@ -9,16 +9,16 @@ $(window).ready(function() {
     }
 });
 
-// $(window).resize(function() {
-//     if ($(window).width() > 1050) {
-//         $('.mobile-nav').css('display','none');
-//         $('#side-menu').show();
-//     }
-//     else {
-//         $('.mobile-nav').css('display','block');
-//         $('#side-menu').hide();
-//     }
-// });
+$(window).resize(function() {
+    if ($(window).width() > 1050) {
+        $('.mobile-nav').css('display','none');
+        $('#side-menu').show();
+    }
+    else {
+        $('.mobile-nav').css('display','block');
+        $('#side-menu').hide();
+    }
+});
 
 $(".mobile-nav").click(function(e){
     e.preventDefault();

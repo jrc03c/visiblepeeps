abde7ee12ed7d22086ec8d1c3d6ab055ea3bd59c
jrc03c <joshrcastle@gmail.com>
2018-08-27 10:08:39 -0500

    Fixed the menu (I hope) and cleaned up some indentation problems.

--- res/js/toggle-menu.js
+++ res/js/toggle-menu.js
@@ -1,27 +1,19 @@
-$(window).ready(function() {
-    if ($(window).width() > 1050) {
-        $('.mobile-nav').css('display','none');
-        $('#side-menu').show();
-    }
-    else {
-        $('.mobile-nav').css('display','block');
-        $('#side-menu').hide();
-    }
-});
+function setCSSRules(){
+	if ($(window).width() > 1050) {
+		$('.mobile-nav').css('display','none');
+		$('#side-menu').show();
+	}
+	else {
+		$('.mobile-nav').css('display','block');
+		$('#side-menu').hide();
+	}
+}
 
-$(window).resize(function() {
-    if ($(window).width() > 1050) {
-        $('.mobile-nav').css('display','none');
-        $('#side-menu').show();
-    }
-    else {
-        $('.mobile-nav').css('display','block');
-        $('#side-menu').hide();
-    }
-});
+$(window).ready(setCSSRules);
+$(window).resize(setCSSRules);
 
 $(".mobile-nav").click(function(e){
-    e.preventDefault();
-    $("#side-menu").slideToggle("fast");   
-      return false;
+	e.preventDefault();
+	$("#side-menu").slideToggle("fast");   
+	return false;
 });

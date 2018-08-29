072c0c8ff6e35100a6d102f658a4f0831b011da5
AWerchmeister <42685208+AWerchmeister@users.noreply.github.com>
2018-08-29 23:17:26 +0200

    s

--- res/js/src/components/about.vue
+++ res/js/src/components/about.vue
@@ -7,17 +7,17 @@
 			
 			<div class="about-page">
 				
-				<p>Visible Peeps is a site where women, non-binary and trans creators of all skill levels can share their tweets from the popular hashtags #VisibleWomen and #VisibleNB on Twitter. These hashtags offer a great way to discover new and talented creators that are vastly under-represented within the creative industry. The goal of the site is to create a place where people can explore tweets, creators and art with ease.</p><br>
+				<p>Visible Peeps is a site where women, non-binary and trans creators of all skill levels can share their tweets from the popular hashtags #VisibleWomen and #VisibleNB on Twitter. Started by the folks at <a href="https://milkfed.us/">Milkfed Criminal Masterminds</a> these hashtags are intended to raise the visiblity of women, non-binary and trans creators. The goal of this site is to facilitate the purpose of the hashtag by creating a place where people can explore and discover new creators and their tweets.</p><br>
 
 				<p>Visible Peeps was created by <a href="https://twitter.com/jrc03c">Josh Castle</a> and <a href="https://twitter.com/A_Werchmeister">Andreas Werchmeister</a>, and with the gracious support and help of <a href="https://twitter.com/LetsWoolgather">Toby Brommerich</a>, <a href="https://twitter.com/kylemadkins">Kyle Adkins</a>, as well as <a href="https://twitter.com/tarakeet">Tara Han-Tran Johnson</a>, who created the beautiful title logo.</p>
 
 				<h3>How to use</h3>
 
-				<p>To submit a tweet, all you have to do is click the 'Submit Tweet' button on the home page, or 'Login' in the menu. Once you have logged in using your Twitter account, you'll be able to submit your tweet by simply copy / pasting the direct URL to your tweet, selecting any categories you want your tweet to appear in, and hit 'Save'. That's it! :D</p><br>
+				<p>To submit a tweet, all you have to do is click the 'Submit Tweet' button on the home page. Once you have logged in using your Twitter account, you'll be able to submit your tweet by simply copy / pasting the direct URL to your tweet, selecting any categories you want your tweet to appear in, and hit 'Save'. That's it! :D</p><br>
 
-				<p>Once you have logged in the first time and been approved, the submission page will act as your profile. You can update your tweet or any of the categories you have selected at any time, or if you wish, delete your profile and stored information (sadface).</p><br>
+				<p>Once you have logged in the first time and been approved. You can return to the submission page to, update your tweet or any of the categories you have selected, or if you wish, delete your tweet together with any information stored in the database.</p><br>
 
-				<p>Note: The reason we have opted for an approval process is solely to prevert malicious misuse of the site.</p>
+				<p>Note: The reason we ask you to login with your Twitter account is solely to prevert malicious misuse of the site.</p>
 
 				<h3>Site issues</h3>
 
@@ -31,7 +31,7 @@
 
 				<p>Copyright: We don't own any artwork contained within the tweets featured on the site. All tweets have been submitted by their respective owners.</p><br>
 					
-				<p>Personal Data: We don't store any personal data. The only information we store are the URL's and the category choices you have provided us. You can delete this at any point.</p>
+				<p>Personal Data: We don't store any personal data. The only information we store are the URL's and the category choices you have provided us. You can delete this at any point from the submission page.</p>
 
 			</div>
 		</div>

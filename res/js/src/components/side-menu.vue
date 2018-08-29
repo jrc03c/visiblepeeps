78a0cb346e75ca65dca8da05bed1df6364f87f22
AWerchmeister <42685208+AWerchmeister@users.noreply.github.com>
2018-08-29 19:11:17 +0200

    s

--- res/js/src/components/side-menu.vue
+++ res/js/src/components/side-menu.vue
@@ -7,8 +7,10 @@
 
 								
 				<!-- <li class="li-heading">ACCOUNT</li> -->
+				
+				<li v-if="$store.state.currentUser"><router-link to="/profile" class="fake-a li-heading">Submit Tweet</router-link></li>
 				<li><a class="fake-a li-fat" @click="logInOrOut">{{ $store.state.currentUser ? "Log out" : "Log in" }}</a></li>
-				<li v-if="$store.state.currentUser"><router-link to="/profile" class="fake-a li-fat">Submit Tweet</router-link></li>
+				
 				<span v-if="isAdmin">
 
 					<li><router-link to="/" class="fake-a">Home</router-link></li>

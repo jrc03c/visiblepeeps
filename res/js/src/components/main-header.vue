7ff4863f587180782cf00bf1891224fb22334811
jrc03c <joshrcastle@gmail.com>
2018-08-30 08:13:59 -0500

    Proofreading...

--- res/js/src/components/main-header.vue
+++ res/js/src/components/main-header.vue
@@ -2,7 +2,7 @@
 	<div>
 		<a href=""><img class="peeps-lettering" src="/res/img/visiblepeeps.png"></a>
 
-		<h2>Women, non-binary and trans creators on Twitter<br> <a href="https://twitter.com/hashtag/VisibleWomen?src=hash">#VisibleWomen</a> · <a href="https://twitter.com/hashtag/VisibleNB?src=hash">#VisibleNB</a></h2>
+		<h2>Women, non-binary, and trans creators on Twitter<br> <a href="https://twitter.com/hashtag/VisibleWomen?src=hash">#VisibleWomen</a> · <a href="https://twitter.com/hashtag/VisibleNB?src=hash">#VisibleNB</a></h2>
 	</div>
 </template>
 

1180719badf27936a89912bc44d211aa271f976c
jrc03c <joshrcastle@gmail.com>
2018-08-30 08:22:16 -0500

    Proofreading...

--- res/js/src/components/profile.vue
+++ res/js/src/components/profile.vue
@@ -8,9 +8,9 @@
 			<div class="submit-page">
 				<div v-if="isLoggedIn">
 					
-					<p>Hi again! We are so happy you want to join! :D Remember you can at any point return to this page to, update your submitted tweet, update any categories you have picked, or if you wish, delete your tweet from the site.</p><br>
+					<p>Hi again! We are so happy that you want to join! :D Remember that you can at any point return to this page to update your submitted tweet, update any categories you have picked, or (if you wish) delete your tweet from the site.</p><br>
 						
-					<p>Please only submit tweets including the following hashtags: <span style="color:rgb(29,161,242);">#VisibleWomen · #VisibleWoman · #VisibleNB · #VisibileNBs</span></p>
+					<p>Please only submit tweets that include the following hashtags: <span style="color:rgb(29,161,242);">#VisibleWomen · #VisibleWoman · #VisibleNB · #VisibileNBs</span></p>
 						
 					<br><br>
 					
@@ -19,7 +19,7 @@
 							<p style="font-weight:500;">Tweet URL</p>
 						</label>
 						
-						<p style="font-style:italic; color:rgb(135,135,135);">This can be found by pressing the downward arrow in the top right corner on your tweet, and then clicking 'Copy link to tweet'.</p>	
+						<p style="font-style:italic; color:rgb(135,135,135);">This can be found by pressing the downward arrow in the top right corner on your tweet, and then clicking "Copy link to tweet."</p>	
 						
 						<input type="text" v-model="url" id="tweet-url" @keydown="message = ''">
 						
@@ -39,8 +39,8 @@
 						
 						<br><br><br><br>
 						
-						<p style="font-weight:500;">Choose the profession that best describes you (Select up to 3)</p>
-						<p style="font-style:italic; color:rgb(135,135,135);">If you feel like a category that represents you is missing, please feel free to contact us, and we will sort it out. You can find contact info on the about page under Site Issues.</p>
+						<p style="font-weight:500;">Choose up to 3 categories that best describe your profession</p>
+						<p style="font-style:italic; color:rgb(135,135,135);">If you feel like a category that represents you is missing, please feel free to contact us, and we will sort it out. You can find contact info on the <router-link to="/about">About</router-link> page under Site Issues.</p>
 						
 						<br>
 						
@@ -64,9 +64,9 @@
 				</div>
 					
 				<div v-else>
-					<p>Hi! Before you can submit your tweet to the site, we need to ask you to login with your Twitter account. This is solely to prevent any malicious misuse of the site, and to verify that the tweet you submit is your own. We do not store any personal data besides your username, and we can't access or control any part your Twitter account.</p><br>
+					<p>Hi! Before you can submit your tweet to the site, we need to ask you to log in with your Twitter account. This is necessary both to prevent any malicious misuse of the site and to verify that the tweet you submit is your own; but we do not store any personal data besides your username, and we can't access or control any part your Twitter account.</p><br>
 
-					<p>Once you have made a submission and been approved, you can at any point return to this page to, update your submitted tweet, update any categories you have picked, or if you wish, delete your tweet from the site.</p><br>
+					<p>Once you have made a submission and been approved, you can at any point return to this page to update your submitted tweet, update any categories you have picked, or (if you wish) delete your tweet from the site.</p><br>
 
 					<button @click="$store.dispatch('login')">Login</button>
 				</div>

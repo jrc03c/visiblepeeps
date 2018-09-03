a9415f81711e9c41b745f0f875f3292055687d16
jrc03c <joshrcastle@gmail.com>
2018-09-03 08:34:15 -0500

    Moved the "attention" message to its own component.

--- res/js/src/components/index.vue
+++ res/js/src/components/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<div id="attention">HI, FRIEND! We <i>just</i> launched the site! If you experience any issues or have any feedback for us, please feel free to contact either <a href="https://twitter.com/jrc03c">jrc03c</a> or <a href="https://twitter.com/A_Werchmeister">A_Werchmeister</a> on Twitter!</div>
+		<attention></attention>
 
 		<side-menu></side-menu>
 		
@@ -32,6 +32,7 @@
 	let _ = require("lodash");
 	require("./side-menu.vue");
 	require("./main-header.vue");
+	require("./attention.vue");
 	
 	let scrollListener;
 	
